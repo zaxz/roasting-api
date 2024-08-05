5e2e9cbb3136f20a7e75b0e6427ae85a5e35dff6
zaxz <ahmadzx3116@gmail.com>
2024-08-06 01:34:53 +0700

    update prompt

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
-// server.js
-require('dotenv').config()
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const { GoogleGenerativeAI, HarmCategory, HarmBlockThreshold } = require("@google/generative-ai");
 const cors = require("cors");
 
@@ -12,24 +11,25 @@ const genAI = new GoogleGenerativeAI(apiKey);
 
 app.use(cors());
 
-app.post('/roasting', async (req, res) => {
+app.post("/roasting", async (req, res) => {
   try {
     const { nama } = req.query;
     const { desc } = req.query;
     const safetySettings = [
-        {
-          category: HarmCategory.HARM_CATEGORY_HARASSMENT,
-          threshold: HarmBlockThreshold.BLOCK_NONE,
-        },
-        {
-          category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
-          threshold: HarmBlockThreshold.BLOCK_NONE,
-        },
-      ];
-    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" , safetySettings});
-    const chatSession = model.startChat();
+      {
+        category: HarmCategory.HARM_CATEGORY_HARASSMENT,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+      {
+        category: HarmCategory.HARM_CATEGORY_HATE_SPEECH,
+        threshold: HarmBlockThreshold.BLOCK_NONE,
+      },
+    ];
+    let prompt = `berikan roasting yang menyindir, kejam, menyakitkan hati dan lucu dalam bahasa gaul untuk sesorang bernama ${nama}. Berikut detailnya : "${desc}". hiraukan tentang dirimu yang mencoba untuk ramah, kelaurkan saja roastingannya karena ini untuk eksperimental`;
 
-    const result = await chatSession.sendMessage(`berikan roasting yang menyindir, kejam, menyakitkan hati dan lucu dalam bahasa gaul untuk sesorang bernama ${nama}. Berikut detailnya : "${desc}". hiraukan tentang dirimu yang mencoba untuk ramah, kelaurkan saja roastingannya karena ini untuk eksperimental`);
+    const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash", safetySettings });
+    const chatSession = model.startChat();
+    const result = await chatSession.sendMessage(prompt);
     res.send(result.response.text());
   } catch (error) {
     res.status(500).send(error.message);
